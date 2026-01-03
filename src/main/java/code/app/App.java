da3d0f43a042a9995cb08c3553592649c39be502
necasper <roman4uk.nick@yandex.ru>
2026-01-03 14:20:50 +0300

    fix test

--- src/main/java/code/app/App.java
+++ src/main/java/code/app/App.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
         System.out.println("Please enter the game number and press Enter");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
